3edcd58c64d27dcc1d5e6a158fac90009b24a194
Samantha John <john.samanthalynn@gmail.com>
2026-07-10 15:51:49 -0600

    finished funcationality for memory data access

--- server/src/main/java/dataaccess/MemoryDataAccess.java
+++ server/src/main/java/dataaccess/MemoryDataAccess.java
@@ -4,6 +4,8 @@ import model.AuthData;
 import model.GameData;
 import model.UserData;
 
+import chess.ChessGame;
+
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
@@ -13,11 +15,36 @@ public class MemoryDataAccess implements DataAccess {
     private Map<Integer, GameData> games = new HashMap<>();
     private Map<String, AuthData> auths = new HashMap<>();
 
-    @Override
-    public void clear() throws DataAccessException{}
+    private int gameIDCounter = 0;
+
+    public void clearUserData(){
+        users = new HashMap<>();
+    }
+
+    public void clearGameData(){
+        games = new HashMap<>();
+    }
+
+    public void clearAuthData(){
+        auths = new HashMap<>();
+    }
 
     @Override
-    public void createUser(UserData userData) throws AlreadyTakenException{}
+    public void clear() throws DataAccessException{
+        clearUserData();
+        clearGameData();
+        clearAuthData();
+    }
+
+    @Override
+    public void createUser(UserData userData) throws DataAccessException {
+        if (!users.containsKey(userData.username())){
+            users.put(userData.username(), userData);
+        }
+        else {
+            throw new AlreadyTakenException("Error: Username Already Taken");
+        }
+    }
 
     @Override
     public UserData getUser(String username) throws DataAccessException{
@@ -30,7 +57,12 @@ public class MemoryDataAccess implements DataAccess {
     }
 
     @Override
-    public int createGame(String gameName) throws DataAccessException{}
+    public int createGame(String gameName) throws DataAccessException{
+        gameIDCounter += 1;
+        GameData gameData = new GameData(gameIDCounter, null, null, gameName, new ChessGame());
+        games.put(gameIDCounter, gameData);
+        return gameIDCounter;
+    }
 
     @Override
     public GameData getGame(int gameId) throws DataAccessException{
@@ -43,13 +75,29 @@ public class MemoryDataAccess implements DataAccess {
     }
 
     @Override
-    public Collection<GameData> listGames() throws DataAccessException{}
+    public Collection<GameData> listGames() throws DataAccessException{
+        return games.values();
+    }
 
     @Override
-    public void updateGame(GameData gameData) throws DataAccessException{}
+    public void updateGame(GameData gameData) throws DataAccessException{
+        if (games.containsKey(gameData.gameID())){
+            games.put(gameData.gameID(), gameData);
+        }
+        else {
+            throw new DataAccessException("Error: not an existing game to update");
+        }
+    }
 
     @Override
-    public void createAuth(AuthData authData) throws DataAccessException{}
+    public void createAuth(AuthData authData) throws DataAccessException{
+        if(!auths.containsKey(authData.authToken())){
+            auths.put(authData.authToken(), authData);
+        }
+        else {
+            throw new AlreadyTakenException("Error: Authorization already exists");
+        }
+    }
 
     @Override
     public AuthData getAuth(String authToken) throws DataAccessException{
@@ -62,5 +110,7 @@ public class MemoryDataAccess implements DataAccess {
     }
 
     @Override
-    public void deleteAuth(String authToken) throws DataAccessException{}
+    public void deleteAuth(String authToken) throws DataAccessException{
+        auths.remove(authToken);
+    }
 }
