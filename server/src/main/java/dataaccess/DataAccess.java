f0bd849cc98debbd5be9a8b405c69a797783c9b7
Samantha John <john.samanthalynn@gmail.com>
2026-07-10 14:52:28 -0600

    added parameters to interface methods

--- server/src/main/java/dataaccess/DataAccess.java
+++ server/src/main/java/dataaccess/DataAccess.java
@@ -9,15 +9,15 @@ import java.util.Collection;
 public interface DataAccess {
     void clear() throws DataAccessException;
 
-    void createUser() throws DataAccessException;
-    UserData getUser() throws DataAccessException;
+    void createUser(UserData userData) throws DataAccessException;
+    UserData getUser(String username) throws DataAccessException;
 
-    int createGame() throws DataAccessException;
-    GameData getGame() throws DataAccessException;
+    int createGame(String gameName) throws DataAccessException;
+    GameData getGame(int gameId) throws DataAccessException;
     Collection<GameData> listGames() throws DataAccessException;
-    void updateGame() throws DataAccessException;
+    void updateGame(GameData gameData) throws DataAccessException;
 
-    void createAuth() throws DataAccessException;
-    AuthData getAuth() throws DataAccessException;
-    void deleteAuth() throws DataAccessException;
+    void createAuth(AuthData authData) throws DataAccessException;
+    AuthData getAuth(String authToken) throws DataAccessException;
+    void deleteAuth(String authToken) throws DataAccessException;
 }
