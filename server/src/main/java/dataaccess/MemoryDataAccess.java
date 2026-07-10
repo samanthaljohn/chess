62f51f2570951ba836cae45d9d62a089ca2f102c
Samantha John <john.samanthalynn@gmail.com>
2026-07-10 15:20:26 -0600

    added getter functionality

--- server/src/main/java/dataaccess/MemoryDataAccess.java
+++ server/src/main/java/dataaccess/MemoryDataAccess.java
@@ -9,20 +9,38 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class MemoryDataAccess implements DataAccess {
+    private Map<String, UserData> users = new HashMap<>();
+    private Map<Integer, GameData> games = new HashMap<>();
+    private Map<String, AuthData> auths = new HashMap<>();
+
     @Override
     public void clear() throws DataAccessException{}
 
     @Override
-    public void createUser(UserData userData) throws DataAccessException{}
+    public void createUser(UserData userData) throws AlreadyTakenException{}
 
     @Override
-    public UserData getUser(String username) throws DataAccessException{}
+    public UserData getUser(String username) throws DataAccessException{
+        if (users.containsKey(username)){
+            return users.get(username);
+        }
+        else {
+            return null;
+        }
+    }
 
     @Override
     public int createGame(String gameName) throws DataAccessException{}
 
     @Override
-    public GameData getGame(int gameId) throws DataAccessException{}
+    public GameData getGame(int gameId) throws DataAccessException{
+        if (games.containsKey(gameId)){
+            return games.get(gameId);
+        }
+        else {
+            return null;
+        }
+    }
 
     @Override
     public Collection<GameData> listGames() throws DataAccessException{}
@@ -34,7 +52,14 @@ public class MemoryDataAccess implements DataAccess {
     public void createAuth(AuthData authData) throws DataAccessException{}
 
     @Override
-    public AuthData getAuth(String authToken) throws DataAccessException{}
+    public AuthData getAuth(String authToken) throws DataAccessException{
+        if (auths.containsKey(authToken)){
+            return auths.get(authToken);
+        }
+        else {
+            return null;
+        }
+    }
 
     @Override
     public void deleteAuth(String authToken) throws DataAccessException{}
