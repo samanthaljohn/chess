c07c71227b6994936e7b06c8990f5b9fab918a19
Samantha John <john.samanthalynn@gmail.com>
2026-07-12 16:46:50 -0600

    implemented register method

--- server/src/main/java/service/UserService.java
+++ server/src/main/java/service/UserService.java
@@ -1,16 +1,47 @@
-//package service;
-//
-//import dataaccess.DataAccess;
-//
-//public class UserService {
-//    private DataAccess dataAccess;
-//
-//    public UserService(DataAccess dataAccess){
-//        this.dataAccess = dataAccess;
-//    }
-//
-//    public RegisterResult register(RegisterRequest registerRequest) {}
+package service;
+
+import dataaccess.AlreadyTakenException;
+import dataaccess.DataAccess;
+import dataaccess.DataAccessException;
+import model.AuthData;
+import model.UserData;
+import request.RegisterRequest;
+import result.RegisterResult;
+
+import java.util.UUID;
+
+public class UserService {
+    private DataAccess dataAccess;
+
+    public UserService(DataAccess dataAccess){
+        this.dataAccess = dataAccess;
+    }
+
+    public static String generateAuthToken() {
+        return UUID.randomUUID().toString();
+    }
+
+    public RegisterResult register(RegisterRequest registerRequest) throws DataAccessException {
+        String username = registerRequest.username(), password = registerRequest.password(), email = registerRequest.email();
+
+        if (dataAccess.getUser(username) != null){
+            throw new AlreadyTakenException("Error: already taken");
+        }
+        UserData userData = new UserData(username, password, email);
+        dataAccess.createUser(userData);
+
+        String authToken = generateAuthToken();
+        if (dataAccess.getAuth(authToken) != null){
+            throw new AlreadyTakenException("Error: already taken");
+        }
+        AuthData authData = new AuthData(authToken, username);
+        dataAccess.createAuth(authData);
+
+        RegisterResult registerResult = new RegisterResult(username, authToken);
+        return registerResult;
+    }
+
 //    public LoginResult login(LoginRequest loginRequest) {}
 //    public void logout(LogoutRequest logoutRequest) {}
-//
-//}
+
+}
