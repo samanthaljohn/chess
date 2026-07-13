c970a92c8e125e03da9800e80fa3f96c5de15f0c
Samantha John <john.samanthalynn@gmail.com>
2026-07-13 17:19:31 -0600

    added join game handler

--- server/src/main/java/handler/GameHandler.java
+++ server/src/main/java/handler/GameHandler.java
@@ -1,6 +1,7 @@
 package handler;
 
 import com.google.gson.Gson;
+import dataaccess.BadRequestException;
 import io.javalin.http.Context;
 
 import dataaccess.AlreadyTakenException;
@@ -8,6 +9,7 @@ import dataaccess.UnauthorizedException;
 import dataaccess.DataAccessException;
 
 import request.CreateGameRequest;
+import request.JoinGameRequest;
 import request.LoginRequest;
 import request.RegisterRequest;
 import result.CreateGameResult;
@@ -64,6 +66,43 @@ public class GameHandler {
             context.result(new Gson().toJson(createGameResult));
         } catch (UnauthorizedException e){
             context.status(401);
+            context.contentType("application/json");
+            context.result(new Gson().toJson(Map.of("message", "Error: " + e.getMessage())));
+        } catch (DataAccessException e){
+            context.status(500);
+            context.contentType("application/json");
+            context.result(new Gson().toJson(Map.of("message", "Error: " + e.getMessage())));
+        }
+    }
+
+    public void joinGame(Context context){
+        String authToken = context.header("authorization");
+        JoinGameRequest joinGameRequest = new Gson().fromJson(context.body(), JoinGameRequest.class);
+
+        String color = joinGameRequest.playerColor();
+
+        if(color == null || (!color.equals("WHITE") && !color.equals("BLACK")) || joinGameRequest.gameID() == 0) {
+            context.status(400);
+            context.contentType("application/json");
+            context.result(new Gson().toJson(Map.of("message", "Error: bad request")));
+            return;
+        }
+
+        try {
+            gameService.joinGame(authToken, joinGameRequest);
+            context.status(200);
+            context.contentType("application/json");
+            context.result(new Gson().toJson(Map.of()));
+        } catch (BadRequestException e){
+            context.status(400);
+            context.contentType("application/json");
+            context.result(new Gson().toJson(Map.of("message", "Error: " + e.getMessage())));
+        } catch (UnauthorizedException e){
+            context.status(401);
+            context.contentType("application/json");
+            context.result(new Gson().toJson(Map.of("message", "Error: " + e.getMessage())));
+        } catch (AlreadyTakenException e){
+            context.status(403);
             context.contentType("application/json");
             context.result(new Gson().toJson(Map.of("message", "Error: " + e.getMessage())));
         } catch (DataAccessException e){
