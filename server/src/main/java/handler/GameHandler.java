5c12f3bd3bb09180ec83eb02b8bd7f7f3645b0b6
Samantha John <john.samanthalynn@gmail.com>
2026-07-13 16:40:16 -0600

    added create game handler

--- server/src/main/java/handler/GameHandler.java
+++ server/src/main/java/handler/GameHandler.java
@@ -7,14 +7,17 @@ import dataaccess.AlreadyTakenException;
 import dataaccess.UnauthorizedException;
 import dataaccess.DataAccessException;
 
+import request.CreateGameRequest;
 import request.LoginRequest;
 import request.RegisterRequest;
+import result.CreateGameResult;
 import result.ListGamesResult;
 import result.LoginResult;
 import result.RegisterResult;
 
 import service.GameService;
 
+import javax.xml.crypto.Data;
 import java.util.Map;
 
 public class GameHandler {
@@ -32,6 +35,33 @@ public class GameHandler {
             context.status(200);
             context.contentType("application/json");
             context.result(new Gson().toJson(listGamesResult));
+        } catch (UnauthorizedException e){
+            context.status(401);
+            context.contentType("application/json");
+            context.result(new Gson().toJson(Map.of("message", "Error: " + e.getMessage())));
+        } catch (DataAccessException e){
+            context.status(500);
+            context.contentType("application/json");
+            context.result(new Gson().toJson(Map.of("message", "Error: " + e.getMessage())));
+        }
+    }
+
+    public void createGame(Context context){
+        String authToken = context.header("authorization");
+        CreateGameRequest createGameRequest = new Gson().fromJson(context.body(), CreateGameRequest.class);
+
+        if (createGameRequest.gameName() == null){
+            context.status(400);
+            context.contentType("application/json");
+            context.result(new Gson().toJson(Map.of("message", "Error: bad request")));
+            return;
+        }
+
+        try {
+            CreateGameResult createGameResult = gameService.createGame(authToken, createGameRequest);
+            context.status(200);
+            context.contentType("application/json");
+            context.result(new Gson().toJson(createGameResult));
         } catch (UnauthorizedException e){
             context.status(401);
             context.contentType("application/json");
