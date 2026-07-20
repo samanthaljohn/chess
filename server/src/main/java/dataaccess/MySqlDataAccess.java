e462079c6f89a307eeb16ab7019217f4b0dafd04
Samantha John <john.samanthalynn@gmail.com>
2026-07-20 15:10:48 -0600

    stubbed out all methods for database implementation of data access

--- server/src/main/java/dataaccess/MySqlDataAccess.java
+++ server/src/main/java/dataaccess/MySqlDataAccess.java
@@ -1,4 +1,33 @@
 package dataaccess;
 
-public class MySqlDataAccess {
+import model.AuthData;
+import model.GameData;
+import model.UserData;
+
+import java.util.Collection;
+
+public class MySqlDataAccess implements DataAccess{
+    public void clear() throws DataAccessException{};
+
+    public void createUser(UserData userData) throws DataAccessException{};
+    public UserData getUser(String username) throws DataAccessException{
+        return null;
+    };
+
+    public int createGame(String gameName) throws DataAccessException{
+        return 0;
+    };
+    public GameData getGame(int gameId) throws DataAccessException{
+        return null;
+    };
+    public Collection<GameData> listGames() throws DataAccessException{
+        return null;
+    };
+    public void updateGame(GameData gameData) throws DataAccessException{};
+
+    public void createAuth(AuthData authData) throws DataAccessException{};
+    public AuthData getAuth(String authToken) throws DataAccessException{
+        return null;
+    };
+    public void deleteAuth(String authToken) throws DataAccessException{};
 }
