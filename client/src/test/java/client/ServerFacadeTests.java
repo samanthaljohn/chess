e33d32c4b00a7ae4cc687ed5eb9447fd5d55f85f
Samantha John <john.samanthalynn@gmail.com>
2026-07-27 16:22:41 -0600

    added register and login tests

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -1,30 +1,68 @@
 package client;
 
 import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import result.LoginResult;
+import result.RegisterResult;
 import server.Server;
-import ServerFacade;
 
 public class ServerFacadeTests {
 
     private static Server server;
+    static ServerFacade facade;
 
     @BeforeAll
     public static void init() {
         server = new Server();
         var port = server.run(0);
         System.out.println("Started test HTTP server on " + port);
+
         facade = new ServerFacade(port);
     }
 
-    @AfterAll
-    static void stopServer() {
-        server.stop();
+    @BeforeEach
+    public void clear() throws Exception{
+        facade.clear();
     }
+
+    @AfterAll
+    static void stopServer() { server.stop(); }
 
 
     @Test
-    public void sampleTest() {
-        Assertions.assertTrue(true);
+    public void registerPositive() throws Exception{
+        RegisterResult registerResult = facade.register("username", "password", "email@email.com");
+
+        assertEquals("username", registerResult.username());
+        assertNotNull(registerResult.authToken());
+        assertTrue(registerResult.authToken().length() > 10);
+    }
+
+    @Test
+    public void registerNegative() throws Exception{
+        RegisterResult registerResult = facade.register("username", "password", "email@email.com");
+
+        assertThrows(ResponseException.class, () -> facade.register("username", "differentPassword", "differentEmail@email.com"));
+    }
+
+    @Test
+    public void loginPositive() throws Exception{
+        facade.register("username", "password", "email@email.com");
+
+        LoginResult loginResult = facade.login("username", "password");
+
+        assertEquals("username", loginResult.username());
+        assertNotNull(loginResult.authToken());
+        assertTrue(loginResult.authToken().length() > 10);
+    }
+
+    @Test
+    public void loginNegative() throws Exception{
+        facade.register("username", "password", "email@email.com");
+
+        assertThrows(ResponseException.class, () -> facade.login("nonexistentUser", "password"));
+        assertThrows(ResponseException.class, () -> facade.login("username", "wrongPassword"));
     }
 
 }
