d3b40ed83dc1c7b614a69b525ba8ee8dbb3cac18
Samantha John <john.samanthalynn@gmail.com>
2026-07-27 16:23:31 -0600

    removed unused import

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -3,7 +3,6 @@ package server;
 import com.google.gson.Gson;
 import dataaccess.DataAccess;
 import dataaccess.DataAccessException;
-import dataaccess.MemoryDataAccess;
 
 import dataaccess.MySqlDataAccess;
 import handler.ClearHandler;
