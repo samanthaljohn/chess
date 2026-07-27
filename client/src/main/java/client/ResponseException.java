50f57fcdb6841605c08698281c1bb1c03c51f846
Samantha John <john.samanthalynn@gmail.com>
2026-07-27 15:26:28 -0600

    made generic exception for client to throw

---
+++ client/src/main/java/client/ResponseException.java
@@ -0,0 +1,14 @@
+package client;
+
+public class ResponseException extends Exception {
+    private int statusCode;
+
+    public ResponseException(String message, int statusCode) {
+        super(message);
+        this.statusCode = statusCode;
+    }
+
+    public int getStatusCode() {
+        return statusCode;
+    }
+}
