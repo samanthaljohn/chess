753546038323aedd548da01a0755cf57d1fde54f
Samantha John <john.samanthalynn@gmail.com>
2026-07-27 18:29:05 -0600

    added join game test

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -125,4 +125,54 @@ public class ServerFacadeTests {
         assertThrows(ResponseException.class, () -> facade.listGames("badAuthToken"));
     }
 
+    @Test
+    public void joinGamePositive() throws Exception{
+        facade.register("username", "password", "email@email.com");
+        LoginResult loginResult = facade.login("username", "password");
+        String authToken = loginResult.authToken();
+
+        CreateGameResult createGameResult = facade.createGame(authToken, "gameOne");
+        int gameID = createGameResult.gameID();
+
+        assertDoesNotThrow(() -> facade.joinGame(authToken, "WHITE", gameID));
+    }
+
+    @Test
+    public void joinGameBadAuth() throws Exception{
+        facade.register("username", "password", "email@email.com");
+        LoginResult loginResult = facade.login("username", "password");
+        String authToken = loginResult.authToken();
+
+        CreateGameResult createGameResult = facade.createGame(authToken, "gameName");
+        int gameID = createGameResult.gameID();
+
+        assertThrows(ResponseException.class, () -> facade.joinGame("badAuthToken", "WHITE", gameID));
+    }
+
+    @Test
+    public void joinGameBadGameID() throws Exception{
+        facade.register("username", "password", "email@email.com");
+        LoginResult loginResult = facade.login("username", "password");
+        String authToken = loginResult.authToken();
+
+        assertThrows(ResponseException.class, () -> facade.joinGame(authToken, "WHITE", 999));
+    }
+
+    @Test
+    public void joinGameBadPlayerColor() throws Exception{
+        facade.register("username", "password", "email@email.com");
+        LoginResult loginResultOne = facade.login("username", "password");
+        String authTokenOne = loginResultOne.authToken();
+
+        facade.register("usernameTwo", "passwordTwo", "emailTwo@email.com");
+        LoginResult loginResultTwo = facade.login("usernameTwo","passwordTwo");
+        String authTokenTwo = loginResultTwo.authToken();
+
+        CreateGameResult createGameResult = facade.createGame(authTokenOne,"gameName");
+        int gameID = createGameResult.gameID();
+
+        facade.joinGame(authTokenOne, "WHITE", gameID);
+
+        assertThrows(ResponseException.class, () -> facade.joinGame(authTokenTwo, "WHITE", gameID));
+    }
 }
