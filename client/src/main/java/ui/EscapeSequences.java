52850cebc4b7239dd9bc5ce02dd908fa71d576da
Samantha John <john.samanthalynn@gmail.com>
2026-07-28 17:20:39 -0600

    added background color for board

--- client/src/main/java/ui/EscapeSequences.java
+++ client/src/main/java/ui/EscapeSequences.java
@@ -46,6 +46,8 @@ public class EscapeSequences {
     public static final String SET_BG_COLOR_YELLOW = SET_BG_COLOR + "226m";
     public static final String SET_BG_COLOR_BLUE = SET_BG_COLOR + "12m";
     public static final String SET_BG_COLOR_MAGENTA = SET_BG_COLOR + "5m";
+    public static final String SET_BG_COLOR_PURPLE = SET_BG_COLOR + "96m";
+    public static final String SET_BG_COLOR_LIGHT_PINK = SET_BG_COLOR + "218m";
     public static final String SET_BG_COLOR_WHITE = SET_BG_COLOR + "15m";
     public static final String RESET_BG_COLOR = UNICODE_ESCAPE + "[49m";
 
