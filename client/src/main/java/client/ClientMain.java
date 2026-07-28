e8c7ea96f7ea3e2672edffa3f184d5678ed361e3
Samantha John <john.samanthalynn@gmail.com>
2026-07-28 17:40:29 -0600

    connecting repl to client

--- client/src/main/java/client/ClientMain.java
+++ client/src/main/java/client/ClientMain.java
@@ -1,10 +1,16 @@
 package client;
 
 import chess.*;
+import ui.Repl;
 
 public class ClientMain {
     public static void main(String[] args) {
-        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
-        System.out.println("♕ 240 Chess Client: " + piece);
+        int port = 8080;
+        if (args.length == 1) {
+            port = Integer.parseInt(args[0]);
+        }
+
+        Repl repl = new Repl(port);
+        repl.preLoginRepl();
     }
 }
