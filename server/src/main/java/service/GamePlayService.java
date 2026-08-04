b1fe119718d8565465b97d5222e47589d41e3a2d
Samantha John <john.samanthalynn@gmail.com>
2026-08-04 14:27:25 -0600

    add connect functionality in service

--- server/src/main/java/service/GamePlayService.java
+++ server/src/main/java/service/GamePlayService.java
@@ -1,31 +1,68 @@
 package service;
 
+import chess.ChessGame;
 import chess.ChessMove;
+import dataaccess.BadRequestException;
 import dataaccess.DataAccess;
 import dataaccess.DataAccessException;
 import dataaccess.UnauthorizedException;
 import jakarta.websocket.Session;
 import model.AuthData;
+import model.GameData;
+import service.connection.Connection;
+import service.connection.ConnectionManager;
 import websocket.commands.UserGameCommand;
 
 public class GamePlayService {
     private final DataAccess dataAccess;
+    private final ConnectionManager connectionManager;
 
-    private void isAuthorized(String authToken) throws DataAccessException {
+    private AuthData isAuthorized(String authToken) throws DataAccessException {
         AuthData auth = dataAccess.getAuth(authToken);
 
         if (auth == null){
             throw new UnauthorizedException("Unauthorized request.");
         }
+        return auth;
+    }
+
+    private Connection createConnection(AuthData auth, GameData game, Session session){
+        String username = auth.username();
+
+        String playerStatus;
+        ChessGame.TeamColor color;
+
+        if (game.whiteUsername().equals(username)){
+            playerStatus = "PLAYER";
+            color = ChessGame.TeamColor.WHITE;
+        } else if (game.blackUsername().equals(username)){
+            playerStatus = "PLAYER";
+            color = ChessGame.TeamColor.WHITE;
+        } else {
+            playerStatus = "OBSERVER";
+            color = null;
+        }
+
+        return new Connection(username, playerStatus, color, session);
     }
 
     public GamePlayService(DataAccess dataAccess){
         this.dataAccess = dataAccess;
+        this.connectionManager = new ConnectionManager();
     }
 
     public void connect(UserGameCommand command, Session session) throws DataAccessException {
         String authToken = command.getAuthToken();
-        isAuthorized(authToken);
+        AuthData authData = isAuthorized(authToken);
+
+        int gameID = command.getGameID();
+        GameData game = dataAccess.getGame(gameID);
+        if (game == null) {
+            throw new BadRequestException("Game does not exist.");
+        } else {
+            Connection connection = createConnection(authData, game, session);
+            connectionManager.addConnection(gameID, connection);
+        }
     }
 
     public void makeMove(UserGameCommand command, Session session) throws DataAccessException {
