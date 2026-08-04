6ef0f12cd9c44cc471ad0719b5e127b56e673854
Samantha John <john.samanthalynn@gmail.com>
2026-08-04 14:06:45 -0600

    add addConnection feature

--- server/src/main/java/service/connection/ConnectionManager.java
+++ server/src/main/java/service/connection/ConnectionManager.java
@@ -2,6 +2,7 @@ package service.connection;
 
 import websocket.messages.ServerMessage;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.HashMap;
@@ -9,12 +10,27 @@ import java.util.HashMap;
 public class ConnectionManager {
     private final Map<Integer, List<Connection>> connections;
 
+    private boolean gameHasConnections(int gameID){
+        if (connections.get(gameID) == null){
+            return false;
+        }
+        return true;
+    }
+
     public ConnectionManager(){
         this.connections = new HashMap<>();
     }
 
     public void addConnection(int gameID, Connection connection){
+        if (gameHasConnections(gameID)) {
+            connections.get(gameID).add(connection);
 
+        } else {
+            List<Connection> newConnectionList = new ArrayList<>();
+            newConnectionList.add(connection);
+
+            connections.put(gameID, newConnectionList);
+        }
     }
 
     public void removeConnection(int gameID, Connection connection){
