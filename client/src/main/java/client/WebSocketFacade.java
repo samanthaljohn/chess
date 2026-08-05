ab01a0ed328cc0acc595711972a5fec555e63996
Samantha John <john.samanthalynn@gmail.com>
2026-08-04 20:25:43 -0600

    removed unused imports

--- client/src/main/java/client/WebSocketFacade.java
+++ client/src/main/java/client/WebSocketFacade.java
@@ -1,12 +1,9 @@
 package client;
 
-import handler.GamePlayHandler;
 import org.eclipse.jetty.websocket.api.Session;
-import service.GamePlayService;
 
 public class WebSocketFacade {
     Session session;
-    GamePlayHandler gamePlayHandler;
 
     public WebSocketFacade(String url) throws ResponseException {
 
