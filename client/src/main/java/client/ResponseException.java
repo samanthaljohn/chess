ea5ab943d917c44189fa3d869dc64c39aa67f802
Samantha John <john.samanthalynn@gmail.com>
2026-08-05 10:37:29 -0600

    added constructor without status code report to be used in websocket facade

--- client/src/main/java/client/ResponseException.java
+++ client/src/main/java/client/ResponseException.java
@@ -3,6 +3,10 @@ package client;
 public class ResponseException extends Exception {
     private int statusCode;
 
+    public ResponseException(String message){
+        super(message);
+    }
+
     public ResponseException(String message, int statusCode) {
         super(message);
         this.statusCode = statusCode;
